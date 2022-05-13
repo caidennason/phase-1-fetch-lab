4c16a64409009fbfac33f89d408b9b5ab901af54
caiden nason <caidenbnason@gmail.com>
2022-05-13 19:21:51 -0400

    commit tree 81d457d698eb3a967f036223b2830bdbb720e25d

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json()) // converts this to an object format that we can read and use
+  .then(books => renderBooks(books))
 }
 
 function renderBooks(books) {
@@ -15,3 +17,13 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+//fetch("http://api.open-notify.org/astros.json")
+//  .then(function (response) {
+ //   return response.json();
+ // })
+ // .then(function (data) {
+  //  console.log(data);
+  //  console.log(`Holy cow! There are ${data["number"]} humans in space.`);
+ // });
